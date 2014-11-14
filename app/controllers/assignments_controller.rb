dff725a4e594f2d7a05022c48e689d862c7a0479
Frank Zhao <frank.zhao@anu.edu.au>
2014-11-14 22:11:32 +1100

    add controllers, views for CRUD

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,19 @@
+class AssignmentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
