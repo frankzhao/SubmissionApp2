f9a3677976c8f3c4cc11f53ed21d52b9de9abe39
Frank Zhao <frank.zhao@anu.edu.au>
2014-11-21 19:37:03 +1100

    Make some small changes after production test

--- restart_server.sh
+++ restart_server.sh
@@ -1,3 +1,2 @@
-git pull
 bundle exec thin stop
-bundle exec thin start --ssl --ssl-verify --ssl-key-file ssl/varese.key --ssl-cert-file ssl/varese.crt -e production -d
+bundle exec thin start --ssl --ssl-key-file ssl/varese.key --ssl-cert-file ssl/varese_pem.crt -e production -d
