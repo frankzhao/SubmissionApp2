b36c32881d8d6e4ae4aa765f07201a84161bc9b1
Frank Zhao <frank.zhao@anu.edu.au>
2014-12-04 22:22:43 +1100

    Create view for plaintext assignment submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,16 @@
 class SubmissionsController < ApplicationController
+  before_filter :require_logged_in
+  respond_to :html
+
   def new
+    @submission = Submission.new
+    @assignment = Assignment.find(params[:assignment_id])
+    @course = @assignment.course
+    if @assignment.kind == "plaintext"
+      render 'new_plaintext'
+    elsif @assignment.kind = "zip"
+      render 'new_zip'
+    end
   end
 
   def create
