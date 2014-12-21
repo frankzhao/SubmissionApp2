9afaf490d1f30af7e6c5a4218a7bf3eb4d587965
Frank Zhao <frank.zhao@anu.edu.au>
2014-12-21 16:56:33 +1100

    Maybe fixes #14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   before_filter :configure_permitted_paramaters, if: :devise_controller?
   
+  def after_sign_in_path_for(resource)
+    "/courses"
+  end
+  
   protected
 
   def configure_devise_params
