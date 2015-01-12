9f1e7daa46a9ad9258ea0ac01bf9b4afe3196157
Frank Zhao <frank.zhao@anu.edu.au>
2015-01-12 17:55:00 +1100

    Fix user show permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,15 @@ class UsersController < ApplicationController
   before_filter :require_logged_in
 
   def show
-
     if params[:id] && User.find_by_id(params[:id])
       @user = User.find_by_id(params[:id])
       unless current_user.is_staff? or current_user == @user
         flash_message :error, "You don't have permission to access that."
+        redirect_to :back
       end
     else
       flash_message :error, "Could not find user with ID=" + params[:id]
+      redirect_to :back
     end
 
     if current_user.is_admin?
