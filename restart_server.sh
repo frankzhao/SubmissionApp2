4e12988a052c0f3f51fc4ced9a0fbc92385384de
Frank Zhao <frank.zhao@anu.edu.au>
2015-02-08 11:05:30 +1100

    Run seed on production

--- restart_server.sh
+++ restart_server.sh
@@ -3,5 +3,6 @@ bin/delayed_job stop
 cp -r ../ssl .
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake db:seed
 bundle exec thin start --ssl --ssl-key-file ssl/varese.key --ssl-cert-file ssl/varese_pem.crt -e production -d
 RAILS_ENV=production bin/delayed_job -n 4 start
