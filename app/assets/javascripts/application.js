616dc0d0de6e3f4e64a230e23a77f862ba86d964
Frank Zhao <frank@frankzhao.net>
2015-05-16 20:44:56 +1000

    Add datatables search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require bootstrap.min
 //= require date
 //= require chart.min
