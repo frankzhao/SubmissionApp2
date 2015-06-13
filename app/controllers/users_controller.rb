0f0cfd0804a0178a104020317a02a22e36a637df
Frank Zhao <frank.zhao@anu.edu.au>
2015-06-13 23:45:57 +1000

    Change root page to user profile instead of courses index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   before_filter :require_logged_in
 
   def show
-    if params[:id] && User.find_by_id(params[:id])
+    if params[:id].nil?
+      @user = current_user
+    else
+      User.find_by_id(params[:id])
       @user = User.find_by_id(params[:id])
       unless current_user.is_staff? or current_user == @user
         flash_message :error, "You don't have permission to access that."
         redirect_to '/'
       end
-    else
-      flash_message :error, "Could not find user with ID=" + params[:id]
-      redirect_to '/'
     end
 
     if current_user.is_admin?
--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root :to => 'courses#index'
+      root :to => 'users#show'
     end
     unauthenticated :user do
       root :to => 'devise/sessions#new', as: :unauthenticated_root
