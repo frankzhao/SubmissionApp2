98d4888c2b655a92fd8faddc2b74bd2166a6249e
Migara Liyanagamage <mliyanagamage@gmail.com>
2017-02-24 11:55:51 +1100

    remove websocket code

--- app/controllers/websocket_controller.rb
+++
@@ -1,7 +0,0 @@
-class WebsocketController < WebsocketRails::BaseController
-  def initialize_session
-    # perform application setup here
-    # controller_store[:message_count] = 0
-    # message = "Executing..."
-  end
-end
