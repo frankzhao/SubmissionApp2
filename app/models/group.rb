ae1793d60301734281f33cdd83617437c8516b09
Migara Liyanagamage <mliyanagamage@gmail.com>
2017-03-28 18:19:47 +1100

    fix legacy data issue

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,6 +8,6 @@ class Group < ApplicationRecord
   
   def get_student_roles
     course_id = self.course.id.to_s
-    (self.students + User.all.select{|u| u.role.to_h[course_id] == "Student" && u.groups.include?(self)}).uniq
+    (self.students + User.all.select{|u| u.role.to_h[course_id] == "Student" && u.becomes(Student).groups.include?(self)}).uniq
   end
 end
