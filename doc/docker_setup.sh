d5fe6b36cd418780c12bd3a5c7cb257eb0b7beed
Migara Liyanagamage <mliyanagamage@gmail.com>
2017-07-25 08:10:46 +1000

    add script for installing docker

---
+++ doc/docker_setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Docker Setup
+
+echo "updating repos"
+sudo apt-get update
+
+echo "installing dependencies"
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+echo "adding repository"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+echo "Updating repositories"
+sudo apt-get update
+
+echo "Installing docker"
+sudo apt-get install docker-ce
+
+echo "Configuring docker"
+sudo groupadd docker
+
+sudo gpasswd -a $USER docker
+
+echo "Installation complete!"
